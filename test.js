22f2b0aa180cd574969673bb447664957415ce2f
ExZyO <momin007@icloud.com>
2026-09-01 00:54:06 +0100

    chore: clean up obsolete test files, backups, duplicate vendor scripts, and unused prototypes

--- test.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); const text = fs.readFileSync('index.html', 'utf8'); const script = text.split('<script type="module">')[1].split('</script>')[0]; fs.writeFileSync('temp.js', script);
--- test_syntax.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); const code = fs.readFileSync('index.html', 'utf8'); const script = code.split('<script type="module">')[1].split('</script>')[0]; fs.writeFileSync('temp.js', script);
