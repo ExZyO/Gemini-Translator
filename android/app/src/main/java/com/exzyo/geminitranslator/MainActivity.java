1c1422dfbbed33ba880058c509b06417a03c4080
ExZyO <momin007@icloud.com>
2026-09-01 09:08:39 +0100

    fix(native): explicitly register NativeAndroidBridgePlugin & NativeEpubMergerPlugin in MainActivity (v6.9.1)

--- android/app/src/main/java/com/exzyo/geminitranslator/MainActivity.java
+++ android/app/src/main/java/com/exzyo/geminitranslator/MainActivity.java
@@ -1,5 +1,13 @@
 package com.exzyo.geminitranslator;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(NativeAndroidBridgePlugin.class);
+        registerPlugin(NativeEpubMergerPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
