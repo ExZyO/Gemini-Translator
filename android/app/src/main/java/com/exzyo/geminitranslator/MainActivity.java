a14ac1296a03f9f2cf6d434f7644488e6a5d6b17
ExZyO <momin007@icloud.com>
2026-09-01 06:19:54 +0100

    fix(android): remove redundant registerPlugin call in MainActivity for modern Capacitor auto-discovery (v6.5.5)

--- android/app/src/main/java/com/exzyo/geminitranslator/MainActivity.java
+++ android/app/src/main/java/com/exzyo/geminitranslator/MainActivity.java
@@ -1,12 +1,7 @@
 package com.exzyo.geminitranslator;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(NativeEpubMergerPlugin.class);
-        super.onCreate(savedInstanceState);
-    }
+    // Custom plugins with @CapacitorPlugin annotation are automatically registered by Capacitor Bridge
 }
